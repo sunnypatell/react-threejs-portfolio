b3de1469ca710128b7a0f1f8cf058a7d76153b86
Sunny Patel <sunnypatel124555@gmail.com>
2024-02-26 15:38:14 -0500

    Update About component with new content and social media buttons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,18 +47,32 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a aspiring software developer with experience in HTML, CSS, and
-        JavaScript, and expertise in frameworks like React, and Bootstrap. I'm a
-        quick learner and to create efficient, scalable, and user-friendly
-        solutions that solve real-world problems. Let's work together to bring
-        our ideas to life!
+        Sunny Patel is a seasoned computer science and software development professional, 
+        bringing over four years of industry expertise to the table. With a solid academic foundation 
+        in computer science and a passion for problem-solving, Sunny is committed to delivering top-tier 
+        solutions to intricate challenges. His portfolio showcases a diverse range of projects and accomplishments, 
+        highlighting his dedication to excellence in every endeavor
       </motion.p>
 
-      <button className="mt-10 px-6 py-3 text-white bg-gradient-to-r from-blue-500 to-purple-500 rounded-md shadow-md hover:bg-gradient-to-r hover:from-blue-600 hover:to-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50" 
-        onClick={() => window.open("https://drive.google.com/file/d/16xHITIKZF4NNFqm6DCBwFGJrQE354OzZ/view?usp=sharing", "_blank")}
-      >
-        <span className="font-semibold">Resume</span>
-      </button>
+      <div className="mt-10 flex flex-wrap gap-10">
+        <button className="mt-4 px-6 py-3 text-white bg-gradient-to-r from-blue-500 to-purple-500 rounded-md shadow-md hover:bg-gradient-to-r hover:from-blue-600 hover:to-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50" 
+          onClick={() => window.open("https://drive.google.com/file/d/16xHITIKZF4NNFqm6DCBwFGJrQE354OzZ/view?usp=sharing", "_blank")}
+        >
+          <span className="font-semibold">Resume</span>
+        </button>
+
+        <button className="mt-4 px-6 py-3 text-white bg-gradient-to-r from-blue-400 to-blue-600 rounded-md shadow-md hover:bg-gradient-to-r hover:from-blue-500 hover:to-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" 
+          onClick={() => window.open("https://www.linkedin.com/in/sunny-patel-30b460204/", "_blank")}
+        >
+          <span className="font-semibold">LinkedIn</span>
+        </button>
+
+        <button className="mt-4 px-6 py-3 text-white bg-gradient-to-r from-gray-600 to-gray-800 rounded-md shadow-md hover:bg-gradient-to-r hover:from-gray-700 hover:to-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50" 
+          onClick={() => window.open("https://github.com/sunnypatel99", "_blank")}
+        >
+          <span className="font-semibold">GitHub</span>
+        </button>
+      </div>
 
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
