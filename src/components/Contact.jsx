f4e2bf4a78485981ce10aea67a56c04aca78788a
Sunny Patel <sunnypatel124555@gmail.com>
2024-02-26 16:51:38 -0500

    Update contact recipient email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -40,9 +40,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Mohit Singh Rajput",
+          to_name: "Sunny Patel",
           from_email: form.email,
-          to_email: "mohit.s.rajput032997@gmail.com",
+          to_email: "sunnypatel124555@gmail.com",
           location: form.location,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
