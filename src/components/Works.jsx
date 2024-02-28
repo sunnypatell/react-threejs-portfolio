df5e37652d36abbe945e51bcdfb2aa67ae74ca5c
sunnypatell <sunnypatel124555@gmail.com>
2024-02-28 18:44:17 -0500

    text allignment for mobile

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -78,29 +78,25 @@ const Works = () => {
 
   return (
     <>
-      {!isMobile && (
-        <motion.div variants={textVariant()}>
-          <p className={`${styles.sectionSubText}`}>Innovative Solutions, Games, and Creations</p>
-          <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-        </motion.div>
-      )}
+      <motion.div variants={textVariant()}>
+        <h3 className={`${styles.sectionSubText} text-center`}>Innovative Solutions, Games, and Creations</h3>
+      </motion.div>
+
+      <motion.div variants={textVariant()}>
+        <h3 className={`${styles.sectionHeadText} text-center`}>Projects.</h3>
+      </motion.div>
 
       {!isMobile && (
         <div className='w-full flex'>
           <motion.p
             variants={fadeIn("", "", 0.1, 1)}
-            className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+            className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px] text-center'
           >
-            Following projects showcase my skills and experience through real-world
-            examples of my work. Each project is briefly described with links to
-            code repositories and live demos in it. It reflects my ability to solve
-            complex problems, work with different technologies, and manage projects
-            effectively.
           </motion.p>
         </div>
       )}
 
-      <div className='mt-20 flex flex-wrap gap-7'>
+      <div className={`${isMobile ? 'grid grid-cols-1 gap-4 place-items-center' : 'flex flex-wrap gap-7'}`}>
         {projects.map((project, index) => (
           <ProjectCard 
             key={`project-${index}`} 
