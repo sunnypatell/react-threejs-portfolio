d7c12932e2ecdf94aa71d0faf07dee3ac4589933
sunnypatell <sunnypatel124555@gmail.com>
2024-03-11 21:39:27 -0400

    Add contact-box "no-select" to prevent highlight when manual globe spin

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -78,7 +78,7 @@ const Contact = () => {
   };
 
   return (
-    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
+    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden no-select`}>
       <motion.div variants={slideIn("left", "tween", 0.2, 1)} className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
