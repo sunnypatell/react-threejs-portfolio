e153d96aecee9c5e9f97852f6f49a5bd0996a5ac
sunnypatell <sunnypatel124555@gmail.com>
2024-10-14 17:57:08 -0400

    added autorotate on landing page pc

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -66,6 +66,7 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          autoRotate={true}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
