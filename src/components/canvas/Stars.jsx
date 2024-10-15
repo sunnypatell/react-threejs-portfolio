c8692ada2e272f470b9f710d1097629950fe29ae
sunnypatell <sunnypatel124555@gmail.com>
2024-10-15 01:34:25 -0400

    optimized contact star component for isMobile config

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,44 +1,60 @@
-import { useState, useRef, Suspense } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist/maath-random.esm";
+"use client"
+
+import { useState, useRef, useEffect } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { Points, PointMaterial, Preload } from "@react-three/drei"
+import * as random from "maath/random/dist/maath-random.esm"
 
 const Stars = (props) => {
-  const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(6000), { radius: 1.2 }));
+  const ref = useRef()
+  const [sphere] = useState(() => random.inSphere(new Float32Array(2000), { radius: 1.2 }))
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
-  });
+    ref.current.rotation.x -= delta / 15
+    ref.current.rotation.y -= delta / 20
+  })
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color='#f272c8'
-          size={0.002}
+          color="#915EFF"
+          size={0.003}
           sizeAttenuation={true}
           depthWrite={false}
         />
       </Points>
     </group>
-  );
-};
+  )
+}
 
 const StarsCanvas = () => {
-  return (
-    <div className='w-full h-auto absolute inset-0 z-[-1]'>
-      <Canvas camera={{ position: [0, 0, 1] }}>
-        <Suspense fallback={null}>
-          <Stars />
-        </Suspense>
+  const [isMobile, setIsMobile] = useState(false)
 
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)")
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange)
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange)
+    }
+  }, [])
+
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Stars />
         <Preload all />
       </Canvas>
     </div>
-  );
-};
+  )
+}
 
-export default StarsCanvas;
+export default StarsCanvas
