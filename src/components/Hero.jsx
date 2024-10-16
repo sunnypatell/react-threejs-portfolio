f21f2209aeab3e3b4c77673e93b3b6ca4435e7fa
sunnypatell <sunnypatel124555@gmail.com>
2024-10-15 23:36:18 -0400

    changed hero Hi, without last name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -104,7 +104,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Sunny Patel</span> <WavingHand />
+            Hi, I'm <span className="text-[#915EFF]">Sunny</span> <WavingHand />
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I'm a <TypewriterText texts={typedItems} />
