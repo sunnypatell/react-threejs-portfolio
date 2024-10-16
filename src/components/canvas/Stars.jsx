a047fed80bdd4726382345b6ffc2c781577da81f
sunnypatell <sunnypatel124555@gmail.com>
2024-10-15 22:22:00 -0400

    fixed THREE.BufferGeometry.computeBoundingSphere(): Computed radius is NaN

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -7,7 +7,7 @@ import * as random from "maath/random/dist/maath-random.esm"
 
 const Stars = (props) => {
   const ref = useRef()
-  const [sphere] = useState(() => random.inSphere(new Float32Array(2000), { radius: 1.2 }))
+  const [sphere] = useState(() => random.inSphere(new Float32Array(2001), { radius: 1.2 }))
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 15
