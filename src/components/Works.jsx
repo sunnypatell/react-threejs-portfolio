f9992fee51d089bdc53368b731937723bcf66ab3
sunnypatell <sunnypatel124555@gmail.com>
2024-10-28 15:47:46 -0400

    Projects title is now in a visible state for mobile cuz of useInView hook

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Tilt } from "react-tilt";
-import { motion } from "framer-motion";
+import { motion, useAnimation, useInView } from "framer-motion";
 
 import { styles } from "../styles";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
+import { fadeIn } from "../utils/motion";
 
 const ProjectCard = ({
   name,
@@ -25,35 +25,35 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
-        <div className='relative w-full h-[230px]'>
+        <div className="relative w-full h-[230px]">
           <img
             src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl"
           />
 
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer transition-all duration-300 hover:shadow-[0_0_10px_rgba(128,0,128,0.7)]'
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer transition-all duration-300 hover:shadow-[0_0_10px_rgba(128,0,128,0.7)]"
             >
               <img
                 src={github}
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
               />
             </div>
           </div>
         </div>
 
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
         </div>
 
-        <div className='mt-4 flex flex-wrap gap-2'>
+        <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag, index) => (
             <p key={index} className={`text-[14px] ${tag.color}`}>
               #{tag.name}
@@ -74,34 +74,64 @@ const ProjectCard = ({
 };
 
 const Works = () => {
-  const isMobile = window.innerWidth <= 768; // Adjust breakpoint as needed
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.2 }); // Adjust amount as needed
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView, mainControls]);
 
   return (
-    <>
-      <motion.div variants={textVariant()}>
+    <section ref={ref}>
+      <motion.div
+        initial="hidden"
+        animate={mainControls}
+        variants={{
+          hidden: { opacity: 0, y: -20 },
+          visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+        }}
+      >
         <h3 className={`${styles.sectionSubText} text-center`}>
           Innovative Creations
         </h3>
       </motion.div>
 
-      <motion.div variants={textVariant()}>
-        <h3 className={`${styles.sectionHeadText} text-center`}>
-          Projects.
-        </h3>
+      <motion.div
+        initial="hidden"
+        animate={mainControls}
+        variants={{
+          hidden: { opacity: 0, y: -20 },
+          visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+        }}
+      >
+        <h3 className={`${styles.sectionHeadText} text-center`}>Projects.</h3>
       </motion.div>
 
       <motion.div>
-        <div className={`${isMobile ? 'grid grid-cols-1 gap-4 place-items-center' : 'flex flex-wrap gap-7'}`}>
+        <div
+          className={`${
+            window.innerWidth <= 768
+              ? "grid grid-cols-1 gap-4 place-items-center"
+              : "flex flex-wrap gap-7"
+          }`}
+        >
           {projects.map((project, index) => (
-            <ProjectCard 
-              key={`project-${index}`} 
-              animate={isMobile ? {} : fadeIn("up", "spring", index * 0.5, 0.75)}
-              {...project} 
+            <ProjectCard
+              key={`project-${index}`}
+              animate={
+                window.innerWidth <= 768
+                  ? {}
+                  : fadeIn("up", "spring", index * 0.5, 0.75)
+              }
+              {...project}
             />
           ))}
         </div>
       </motion.div>
-    </>
+    </section>
   );
 };
 
