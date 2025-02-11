8c0bfe68c9866fd9b2b8cefa32d669b1177c4d79
sunnypatell <sunnypatel124555@gmail.com>
2025-02-11 14:19:38 -0500

    Vercel Web Analytics import

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
