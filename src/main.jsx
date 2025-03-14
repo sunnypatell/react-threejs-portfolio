321d04696e33965a9d40f98874a57f380059ccd9
sunnypatell <sunnypatel124555@gmail.com>
2025-03-14 17:53:03 -0400

    purged @vercel/speed-insights, chore!(run build)

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Analytics /> {/* To use vercel analytics in layout */}
-    <SpeedInsights />
   </React.StrictMode>,
 )
